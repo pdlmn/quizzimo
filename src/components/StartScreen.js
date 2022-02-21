686b8b4fd15288c0a59dfe60b58e39e268a917d5
pdlmn <djeinwings@gmail.com>
2022-02-21 23:53:01 +0200

    Added cool background gradient and styles, added StartScreen

---
+++ src/components/StartScreen.js
@@ -0,0 +1,12 @@
+const StartScreen = ({ onGameStart }) => {
+  
+  return (
+    <div className="start-screen">
+      <h1>Quizzimo</h1>
+      <p>Test your knowledge!</p>
+      <button>Start quizz</button>
+    </div>
+  )
+}
+
+export default StartScreen
